03192c837f43644c0aac5b0df517fb86a0f2c14d
nicolas.kerkhofs@student.uclouvain.be <nicolas.kerkhofs@student.uclouvain.be>
2022-05-06 10:20:37 +0200

    adding anova tests (legal way ?)

--- utility.py
+++ utility.py
@@ -3,6 +3,9 @@ import pandas as pd
 import time
 import random
 import os
+from load_data import retrieve_cv_metric
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 """
 File containing small utility functions
@@ -23,12 +26,12 @@ def average_dicos(dicos):
     for key in dicos[0]:
         counter[key] = 0
         dico[key] = 0
-        for dictio in dicos :
-            if key in dictio :
+        for dictio in dicos:
+            if key in dictio:
                 dico[key] += dictio[key]
                 counter[key] += 1
-        if counter[key] > 0 :
-            dico[key] = dico[key]/counter[key]
+        if counter[key] > 0:
+            dico[key] = dico[key] / counter[key]
 
     return dico
 
@@ -53,22 +56,25 @@ def compute_p_val_bootstrap(df_bootstrap, df_group_results):
     for modality in df_bootstrap:
         gv = df_group_results[modality][0]
         count = len([v for v in df_bootstrap[modality] if v > gv])
-        pvals[modality] = ((count+1)/(len(df_bootstrap[modality])+1)) * 4
+        pvals[modality] = ((count + 1) / (len(df_bootstrap[modality]) + 1)) * 4
     return pvals
 
 
 def verbose_dataframe(df, subjects_ids, compare=False):
     column_names = ["Modality", "Region", "Score", "Score_mean_dev"]
-    if compare :
+    if compare:
         column_names = ["Analysis", "Score", "Score_mean_dev"]
     vb_df = pd.DataFrame(columns=column_names)
-    for entry in df :
+    for entry in df:
         keywords = entry.split('_')
 
-        if "vis" in keywords : mod = "Vision"
-        elif "aud" in keywords : mod = "Audition"
-        else: mod = "Cross-modal"
-        
+        if "vis" in keywords:
+            mod = "Vision"
+        elif "aud" in keywords:
+            mod = "Audition"
+        else:
+            mod = "Cross-modal"
+
         region = "V5 " if "V5" in keywords else "PT "
         region += "L" if "L" in keywords else "R"
 
@@ -76,27 +82,27 @@ def verbose_dataframe(df, subjects_ids, compare=False):
 
         n = np.sqrt(len(df[entry]) - df[entry].isnull().sum())
         avg = np.mean(df[entry])
-        
+
         for i in subjects_ids:
             if df[entry][i]:
                 new_entry = dict()
-                if compare :
+                if compare:
                     new_entry["Analysis"] = analysis
-                else :
+                else:
                     new_entry["Modality"] = mod
                     new_entry["Region"] = region
                 new_entry["Score"] = df[entry][i]
-                new_entry["Score_mean_dev"] = df[entry][i]/n + avg - avg/n
+                new_entry["Score_mean_dev"] = df[entry][i] / n + avg - avg / n
                 vb_df = vb_df.append(new_entry, ignore_index=True)
 
     return vb_df
 
 
 def cfm_string_to_matrix(cfm_string):
-    if pd.isna(cfm_string) : return [[np.nan]]
-    cf = cfm_string.replace("[","").replace("]","").replace("\n","")
+    if pd.isna(cfm_string): return [[np.nan]]
+    cf = cfm_string.replace("[", "").replace("]", "").replace("\n", "")
     cf = cf.split('.')[:-1]
-    cf = np.asarray(list(map(int, cf))).reshape(4,4)
+    cf = np.asarray(list(map(int, cf))).reshape(4, 4)
     return cf
 
 
@@ -109,10 +115,68 @@ def compute_group_confusion_matrix(df_cf_matrixes, subjects_ids):
         for i, subj_id in enumerate(subjects_ids):
             cfm = cfm_string_to_matrix(df_cf_matrixes[modality][subj_id])
             if not pd.isna(cfm[0][0]):
-                cfm = cfm/np.sum(cfm)*400
+                cfm = cfm / np.sum(cfm) * 400
                 for j in range(4):
                     for k in range(4):
                         gcf[j][k][l] = cfm[j][k]
                 l += 1
         group_cf[modality] = gcf
     return group_cf
+
+
+def compute_anova(folder_base, folder_candidate):
+    base_df = retrieve_cv_metric(folder_base, "accuracy")
+    base_df = base_df[base_df.columns.drop(list(base_df.filter(regex='cross')))]
+    base_df = base_df[base_df.columns.drop(list(base_df.filter(regex='vis_PT')))]
+    base_df = verbose_dataframe(base_df, range(1, 24), compare=True)
+    base_df["dataset"] = np.repeat(["base"], 23 * 6)
+    base_df.dropna(inplace=True)
+    base_df.drop("Score_mean_dev", axis=1, inplace=True)
+    candidate_df = retrieve_cv_metric(folder_candidate, "accuracy")
+    candidate_df = candidate_df[candidate_df.columns.drop(list(candidate_df.filter(regex='cross')))]
+    candidate_df = candidate_df[candidate_df.columns.drop(list(candidate_df.filter(regex='vis_PT')))]
+    candidate_df = verbose_dataframe(candidate_df, range(1, 24), compare=True)
+    candidate_df["dataset"] = np.repeat(["candidate"], 23 * 6)
+    candidate_df.dropna(inplace=True)
+    candidate_df.drop("Score_mean_dev", axis=1, inplace=True)
+
+    df = base_df.append(candidate_df)
+
+    # Performing two-way ANOVA
+    model = ols('Score ~ C(dataset) +C(Analysis):C(dataset)', data=df).fit()
+    results = sm.stats.anova_lm(model, typ=2)
+
+    print(results)
+
+
+def compute_anova_demeaned(folder_base, folder_candidate):
+    base_df = retrieve_cv_metric(folder_base, "accuracy")
+    base_df = base_df[base_df.columns.drop(list(base_df.filter(regex='cross')))]
+    base_df = base_df[base_df.columns.drop(list(base_df.filter(regex='vis_PT')))]
+
+    candidate_df = retrieve_cv_metric(folder_candidate, "accuracy")
+    candidate_df = candidate_df[candidate_df.columns.drop(list(candidate_df.filter(regex='cross')))]
+    candidate_df = candidate_df[candidate_df.columns.drop(list(candidate_df.filter(regex='vis_PT')))]
+
+    tmp_df = base_df.append(candidate_df)
+    mean_vector = tmp_df.mean(axis=0)
+    base_df = base_df.sub(mean_vector, axis=1)
+    candidate_df = candidate_df.sub(mean_vector, axis=1)
+
+    base_df = verbose_dataframe(base_df, range(1, 24), compare=True)
+    base_df["dataset"] = np.repeat(["base"], 23 * 6)
+    base_df.dropna(inplace=True)
+    base_df.drop("Score_mean_dev", axis=1, inplace=True)
+
+    candidate_df = verbose_dataframe(candidate_df, range(1, 24), compare=True)
+    candidate_df["dataset"] = np.repeat(["candidate"], 23 * 6)
+    candidate_df.dropna(inplace=True)
+    candidate_df.drop("Score_mean_dev", axis=1, inplace=True)
+
+    df = base_df.append(candidate_df)
+
+    # Performing two-way ANOVA
+    model = ols('Score ~ C(dataset)', data=df).fit()
+    results = sm.stats.anova_lm(model, typ=2)
+
+    print(results)
